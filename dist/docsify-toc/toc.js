784b7c6c15ec439717e5365616a63f0333857106
XHS-12302 <xhsgg12302@gmail.com>
2024-06-03 04:06:54 +0800

    update something

--- dist/docsify-toc/toc.js
+++ dist/docsify-toc/toc.js
@@ -175,6 +175,21 @@ function plugin(hook, vm) {
       return;
     }
 
+    var os = function() {
+      var ua = navigator.userAgent,
+          isWindowsPhone = /(?:Windows Phone)/.test(ua),
+          isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+          isAndroid = /(?:Android)/.test(ua),
+          isFireFox = /(?:Firefox)/.test(ua),
+          isChrome = /(?:Chrome|CriOS)/.test(ua),
+          isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox &&
+              /(?:Tablet)/.test(ua)),
+          isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+          isPc = !isPhone && !isAndroid && !isSymbian;
+      return { isTablet: isTablet, isPhone: isPhone, isAndroid: isAndroid, isPc: isPc};
+    }();
+    if(!os.isPc) {return;}
+
     // add by 12302 quit with <title> 404...
     var result = document.evaluate('//*[@id="main"]/title', document).iterateNext();
     if (result) {
